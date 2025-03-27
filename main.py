7bd1fd8d31c12167271638dfa5e1c9a75123bdb8
Shahd-A-Mahmoud <127022375+Shahd-A-Mahmoud@users.noreply.github.com>
2025-03-27 06:39:45 +0200

    Update combo + Alarm

--- main.py
+++ main.py
@@ -25,32 +25,32 @@ class MainWindow(QMainWindow):
         self.upload_button.clicked.connect(self.upload_data)
         # fix the icon sizes
         icon_upload = QIcon("Deliveriables/upload (1).ico")
-        pixmap_upload = icon_upload.pixmap(128, 128)  # Force a larger resolution from ICO file
+        pixmap_upload = icon_upload.pixmap(128, 128)  
         self.upload_button.setIcon(QIcon(pixmap_upload))
         self.upload_button.setIconSize(QSize(80, 80))
 
 
         self.HeartRateIcon = self.findChild(QPushButton,"HeartRateIcon")
         icon_heart = QIcon("Deliveriables/heartrate.ico")
-        pixmap_heart = icon_heart.pixmap(128, 128)  # Force a larger resolution from ICO file
+        pixmap_heart = icon_heart.pixmap(128, 128)  
         self.HeartRateIcon.setIcon(QIcon(pixmap_heart))
         self.HeartRateIcon.setIconSize(QSize(90, 90))
 
         self.pressureIcon = self.findChild(QPushButton, "pressureIcon")
         icon_pressure = QIcon("Deliveriables/pressure.ico")
-        pixmap_pressure = icon_pressure.pixmap(256, 256)  # Force a larger resolution from ICO file
+        pixmap_pressure = icon_pressure.pixmap(256, 256)  
         self.pressureIcon.setIcon(QIcon(pixmap_pressure))
         self.pressureIcon.setIconSize(QSize(160, 160))
 
         self.spo2Icon = self.findChild(QPushButton, "spo2Icon")
         icon_spo2 = QIcon("Deliveriables/spo2 (2).ico")
-        pixmap_spo2 = icon_spo2.pixmap(256, 256)  # Force a larger resolution from ICO file
+        pixmap_spo2 = icon_spo2.pixmap(256, 256)  
         self.spo2Icon.setIcon(QIcon(pixmap_spo2))
         self.spo2Icon.setIconSize(QSize(150, 150))
 
         self.temperatureIcon = self.findChild(QPushButton, "TempIcon")
         icon_temp = QIcon("Deliveriables/temperature (1).ico")
-        pixmap_temp = icon_temp.pixmap(256, 256)  # Force a larger resolution from ICO file
+        pixmap_temp = icon_temp.pixmap(256, 256)  
         self.temperatureIcon.setIcon(QIcon(pixmap_temp))
         self.temperatureIcon.setIconSize(QSize(1000, 1000))
 
@@ -58,7 +58,7 @@ class MainWindow(QMainWindow):
 
         self.alarmButton = self.findChild(QPushButton, "AlarmButton")
         icon_alarm = QIcon("Deliveriables/9-removebg-preview.ico")
-        pixmap_alarm = icon_alarm.pixmap(256, 256)  # Force a larger resolution from ICO file
+        pixmap_alarm = icon_alarm.pixmap(256, 256)  
         self.alarmButton.setIcon(QIcon(pixmap_alarm))
         self.alarmButton.setIconSize(QSize(1000, 1000))
 
@@ -66,7 +66,7 @@ class MainWindow(QMainWindow):
 
         # Initialize the alarm sound player
         self.alarmPlayer = QMediaPlayer()
-        alarm_sound_url = QUrl.fromLocalFile("Deliveriables/alarm.mp3")  # change to your file path
+        alarm_sound_url = QUrl.fromLocalFile("Deliveriables/alarm.mp3")  
         self.alarmPlayer.setMedia(QMediaContent(alarm_sound_url))
          # Flag make it open if there is an aryh detected
         self.alarmPlaying = False
@@ -176,22 +176,22 @@ class MainWindow(QMainWindow):
     def toggle_alarm(self):
 
         if self.alarmPlayer.state() == QMediaPlayer.PlayingState:
-            # pause or stop the alarm sound
-            self.alarmPlayer.pause()  # or .stop() if you want to reset playback
+            # pause / stop
+            self.alarmPlayer.pause()  
             self.alarmPlaying = False
 
             off_icon = QIcon("Deliveriables/DeAlarm.ico")
             self.alarmButton.setIcon(off_icon)
-            pixmap_of_alarm = off_icon.pixmap(256, 256)  # Force a larger resolution from ICO file
+            pixmap_of_alarm = off_icon.pixmap(256, 256)  
             self.alarmButton.setIcon(QIcon(pixmap_of_alarm))
             self.alarmButton.setIconSize(QSize(1000, 1000))
         else:
 
             self.alarmPlayer.play()
             self.alarmPlaying = True
-            # Change icon to show alarm on
+            # on
             on_icon = QIcon("Deliveriables/9-removebg-preview.ico")
-            pixmap_on_alarm = on_icon.pixmap(256, 256)  # Force a larger resolution from ICO file
+            pixmap_on_alarm = on_icon.pixmap(256, 256)  
             self.alarmButton.setIcon(QIcon(pixmap_on_alarm))
             self.alarmButton.setIconSize(QSize(1000, 1000))
 
